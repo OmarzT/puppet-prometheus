b1df018e6e637f7cff63d160e9fff99b87de2a3a
Omar Tamer <otamer@tripadvisor.com>
2021-01-27 21:29:01 +0000

    fix

--- manifests/nginxinc_exporter.pp
+++ manifests/nginxinc_exporter.pp
@@ -69,7 +69,7 @@ class prometheus::nginxinc_exporter (
   String[1]        $group        = 'nginxinc-exporter',
   Array[String[1]] $extra_groups = [],
 
-  String[1]        $bin_dir    = '/opt'
+  String[1]        $bin_dir    = '/opt',
   String[1]        $bin_name   = 'nginx-prometheus-exporter'
 
   # service related options
