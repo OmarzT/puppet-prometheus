c63a26e357bd293b580cbc0faf4cdde0f37ca4af
Omar Tamer <otamer@tripadvisor.com>
2021-01-27 15:03:57 +0000

    fix

--- manifests/nginxinc_exporter.pp
+++ manifests/nginxinc_exporter.pp
@@ -52,7 +52,7 @@
 #   Labels to configure on the scrape job. If not set, the `prometheus::daemon` default (`{ 'alias' => $scrape_host }`) will be used.
 class prometheus::nginxinc_exporter (
   # Installation options
-  Enum['url','package'] $install_method   = 'package',
+  Enum['url','package'] $install_method   = 'url',
   Optional[Stdlib::HTTPUrl] $download_url = undef,
   Stdlib::HTTPUrl $download_url_base      = 'https://github.com/nginxinc/nginx-prometheus-exporter/releases',
   String $download_extension              = 'tar.gz',
